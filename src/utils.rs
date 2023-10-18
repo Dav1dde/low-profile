280c1b6df9736a2c4ed6f025debd5e48c740e519
David Herberth <github@dav1d.de>
2023-10-18 22:05:03 +0200

    low-profile: optimizes write fmt sink implementation

--- src/utils.rs
+++ src/utils.rs
@@ -1,21 +1,68 @@
 use core::{fmt, fmt::Debug};
 
+use futures_util::FutureExt;
 use heapless::Vec;
 
 use crate::Write;
 
 /// Adapter to use the `write!()` macro with the async `Write` trait.
 ///
-/// This carries an internal buffer size of 128 bytes. Attempting to write
-/// more than this will always fail.
-pub(crate) trait WriteExt: Write {
+/// This carries an internal overflow buffer of 128 bytes.
+/// Writes exceeding the buffer may fail.
+pub(crate) trait WriteExt: Write
+where
+    Self: Sized,
+{
     async fn write_fmt(
         &mut self,
         fmt: fmt::Arguments<'_>,
     ) -> Result<(), WriteFmtError<Self::Error>> {
-        let mut buf = Vec::<u8, 128>::new();
-        fmt::write(&mut buf, fmt).map_err(|_| WriteFmtError::FmtError)?;
-        self.write_all(&buf).await?;
+        struct ImmediateWrite<'a, T, const SIZE: usize = 128> {
+            sink: &'a mut T,
+            spill: Vec<u8, SIZE>,
+        }
+
+        impl<'a, T: Write> fmt::Write for ImmediateWrite<'a, T> {
+            fn write_str(&mut self, data: &str) -> fmt::Result {
+                let data = data.as_bytes();
+
+                let fut = self.sink.write(data);
+                let fut = core::pin::pin!(fut);
+
+                let spill = if self.spill.is_empty() {
+                    // See if data can be immediately written,
+                    // if it fails (future returns pending), use the spill buffer instead.
+                    match fut.now_or_never() {
+                        // TODO: carry error out somehow, probably through self.error
+                        Some(Err(_todo)) => return Err(fmt::Error),
+                        Some(Ok(size)) => &data[size..],
+                        None => data,
+                    }
+                } else {
+                    data
+                };
+
+                self.spill.extend_from_slice(spill).map_err(|_| fmt::Error)
+            }
+        }
+
+        impl<'a, T: Write> ImmediateWrite<'a, T> {
+            async fn consume(self) -> Result<(), T::Error> {
+                if self.spill.is_empty() {
+                    return Ok(());
+                }
+
+                self.sink.write(&self.spill).await.map(|_| ())
+            }
+        }
+
+        let mut iw = ImmediateWrite {
+            sink: self,
+            spill: Default::default(),
+        };
+
+        fmt::write(&mut iw, fmt).map_err(|_| WriteFmtError::FmtError)?;
+        iw.consume().await?;
 
         Ok(())
     }
